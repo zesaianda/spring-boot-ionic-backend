2719940cebc2b4acc489c56cb7034ab5708eb791
José Eduardo Saianda <zesaianda@hotmail.com>
2019-04-12 16:07:39 +0100

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -30,6 +33,7 @@ public class ItemPedido implements Serializable{
 	}
 	
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
@@ -39,6 +43,7 @@ public class ItemPedido implements Serializable{
 		return id.getProduto();
 	}
 
+	
 	public ItemPedidoPK getId() {
 		return id;
 	}
--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -4,13 +4,17 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
@@ -38,6 +42,4 @@ public class PagamentoComBoleto extends Pagamento {
 	public void setDataPagamento(Date dataPagamento) {
 		this.dataPagamento = dataPagamento;
 	}
-
-		
 }
