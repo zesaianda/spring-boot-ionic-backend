d383325a281e39683edccde36290b5df1d2fc816
zesaianda <zesaianda@hotmail.com>
2019-04-30 20:04:40 +0100

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
@@ -25,7 +25,7 @@ public abstract class AbstractEmailService implements EmailService{
 		sm.setFrom(sender);
 		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
 		sm.setSentDate(new Date(System.currentTimeMillis()));
-		sm.setSubject(obj.toString());
+		sm.setText(obj.toString());
 		return sm;
 	}
 }
